c721884cb757dbc09c75ed04e94007feee61f152
Rafaelteppa <167258743+Rafaelteppa@users.noreply.github.com>
2024-05-22 01:02:29 +0000

    fix: inicio

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div>
+    <h1 className="text-center">BEM VINDO A BIBLIOTECA</h1>
+    <Button>Login</Button>
+    <Button variant="destructive">Excluir</Button>
+    <Button variant="secondary">Cadastrar Livro</Button>
+ 
+    </div>
+)
+
+}
+    
+ 
