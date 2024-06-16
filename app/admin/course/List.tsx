e0611d98ede3aa5b9faec3fd167ee0f9b8446bc8
Rafaelteppa <167258743+Rafaelteppa@users.noreply.github.com>
2024-06-16 19:58:14 +0000

    fix: button

--- app/admin/course/List.tsx
+++ app/admin/course/List.tsx
@@ -1,51 +1,65 @@
+import { Button } from "@/components/ui/button"
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
-import { revalidatePath } from "next/cache";
-  
-  
-  interface IStudent {
-    id:number,
-    name:string,
-    email:string
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { revalidatePath } from "next/cache"
+
+interface ICourse {
+  id: number,
+  name: string
+  email: string
+}
+
+export default async function Listcourse() {
+  const courses = await list()
+  async function list() {
+    revalidatePath("/admin/course")
+    const response = await fetch("https://server20241-beige.vercel.app/courses")
+    return response.json();
+
   }
 
-  export default async function ListStudent() {
-    const students = await list()
-    async function list(){
-     revalidatePath("/admin/student")
-     const response = await fetch("https://apiserver20241.vercel.app/students");
-     return response.json();
-    }
+  async function deleteCourse(formData: FormData){
+    "use server"
+    const id = formData.get("id") as string;
+    const response = await fetch("https://server20241-beige.vercel.app/courses/"+id, {method: "DELETE" });
+    revalidatePath("/admin/course")
+  }
 
-    return (
-      <Table>
-        <TableCaption>Lista de Estudantes</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">ID</TableHead>
-            <TableHead>Nome</TableHead>
-            <TableHead>Email</TableHead>
+  return (
+    <Table>
+      <TableCaption>Lista de cursos</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">ID</TableHead>
+          <TableHead>Nome</TableHead>
+          <TableHead>Ação</TableHead>
+
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {courses.map((item: ICourse) => (
+          <TableRow key={item.id}>
+            <TableCell className="font-medium">{item.id}</TableCell>
+            <TableCell>{item.name}</TableCell>
+
+            <TableCell>
+              <form>
+                <input type="text" name="id" hidden value={item.id} />
+                <Button formAction={deleteCourse} variant="destructive">Excluir</Button>
+              </form>
+            </TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody>
-          {students.map((item:IStudent) => (
-            <TableRow key={item.id}>
-              <TableCell className="font-medium">{item.id}</TableCell>
-              <TableCell>{item.name}</TableCell>
-              <TableCell>{item.email}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-       
-      </Table>
-    )
-  }
-  
+        ))}
+      </TableBody>
+
+    </Table >
+  )
+}
